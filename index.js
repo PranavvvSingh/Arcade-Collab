6a39b66623868fe9fc979bdc17fd36063c430051
PranavvvSingh <pranavvalt@gmail.com>
2023-06-11 17:53:26 +0530

    link

--- index.js
+++ index.js
@@ -5,15 +5,32 @@ app.use(express.json())
 app.use(express.urlencoded({
     extended:true
 }))
+app.set("view engine","ejs")
 app.use("/",express.static("public"))
 app.use("/flappy",express.static("public-flappy"))
 app.use("/galaxy",express.static("public-galaxy"))
 app.use("/pacman",express.static("public-pacman"))
+
 app.use("/signup",signup)
+// do you want to serve signup page directly?
+
+// app.use("/midflappy",express.static("public-signup"))
+// app.use("/midgalaxy",express.static("public-signup"))
+// app.use("/midpacman",express.static("public-signup"))
 
 app.get("/",function(req,res){
     // res.sendFile(__dirname+"/public/index.html");
     res.sendFile(index.html)
+})
+
+app.get("/midflappy",function(req,res){
+    res.render("signup",{game:"flappy"})
+})
+app.get("/midgalaxy",function(req,res){
+    res.render("signup",{game:"galaxy"})
+})
+app.get("/midpacman",function(req,res){
+    res.render("signup",{game:"pacman"})
 })
 
 app.get("/flappy",function(req,res){
